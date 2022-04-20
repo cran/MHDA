357a40e814bc5d9219b31c7c221f374f8e88774f
Yi Ya <Yi.YA_yaya@hotmail.com>
2022-04-20 21:02:29 +0000

    version 1.1

--- R/Steps.analysis.R
+++ R/Steps.analysis.R
@@ -1,7 +1,6 @@
-Steps.analysis<-function(ID,Tag,S,Rhythm1,Rhythm2,Rhythm3,Rhythm4,Rhythm5,Rhythm6,Rhythm7,Start,plot) {
+Steps.analysis<-function(ID,Tag,S,Rhythms,Start,plot,pick.plot) {
 
-    Rhythms<-c(Rhythm1, Rhythm2, Rhythm3, Rhythm4, Rhythm5, Rhythm6, Rhythm7)
-    IDS<-c("Rhythm1","Rhythm2","Rhythm3","Rhythm4","Rhythm5","Rhythm6","Rhythm7")[1:length(Rhythms)]
+    IDS<-paste("Rhythm",1:length(Rhythms),sep="")
     IDS.new<-vector()
     which.Rhythm<-which(1:length(Rhythms) %in% substr(Start,start=1,stop=1))
     IDS.new[1]<-IDS[which.Rhythm]
@@ -31,7 +30,7 @@ Steps.analysis<-function(ID,Tag,S,Rhythm1,Rhythm2,Rhythm3,Rhythm4,Rhythm5,Rhythm
     names(Rhythms.mean)<-IDS
     if(plot==TRUE) {
          Cols<-rep(1:length(Rhythms),length.out=length(LL))    
-         plot(LL,col=Cols,xlab=Tag,ylab="",type="n",main=ID)
+         plot(LL,col=Cols,xlab=Tag,ylab="",type="n",main=ID)      
          legend("topleft",legend=IDS.new[1:length(Rhythms)],col=levels(factor(Cols)),lwd=5)
          for(i in 1:length(LL)) lines(x=c(i,i),y=c(0,LL[i]),col=Cols[i],lwd=5)
          for(j in 1:length(Rhythms)) {
@@ -39,6 +38,32 @@ Steps.analysis<-function(ID,Tag,S,Rhythm1,Rhythm2,Rhythm3,Rhythm4,Rhythm5,Rhythm
                abline(h=Rhythms.mean[ix],lty=2,lwd=2,col=levels(factor(Cols))[j])
          }
     }
+    if(!is.null(pick.plot)) {
+         LL.pick<-NULL
+         Cols.pick<-NULL
+         Cols.t<-vector()
+         P<-vector()
+         for(i in 1:length(pick.plot)) {
+               LL.t<-LL[which(IDS.new==paste("Rhythm",pick.plot[i],sep=""))]
+               LL.pick<-c(LL.pick,LL.t)
+               col.t<-which(IDS.new==paste("Rhythm",pick.plot[i],sep=""))[1]
+               Cols.pick<-c(Cols.pick,rep(col.t,length(LL.t)))
+               P[i]<-length(LL.t)
+               Cols.t[i]<-col.t
+         }    
+         plot(LL.pick,col=Cols.pick,xlab=Tag,ylab="",xaxt="n",type="n",main="")
+         legend("topleft",legend=paste("Rhythm",pick.plot,sep=""),col=Cols.t,lwd=5)
+         for(i in 1:length(LL.pick)) lines(x=c(i,i),y=c(0,LL.pick[i]),col=Cols.pick[i],lwd=5)
+         for(j in 1:length(pick.plot)) {
+               lines(x=c(sum(P[1:j])+0.5,sum(P[1:j])+0.5),y=c(0,max(LL.pick[!is.na(LL.pick)])),col=1,lwd=1,lty=2)
+               axis(1,at=sum(P[1:j]),labels=P[j])
+               ix<-which(names(Rhythms.mean)==paste("Rhythm",pick.plot[j],sep=""))
+               lines(x=c(sum(P[1:j])+0.5-P[j],sum(P[1:j])+0.5),y=c(Rhythms.mean[ix],Rhythms.mean[ix]),col=Cols.t[j],lwd=2,lty=2)
+               if(j!=1) text(x=sum(P[1:j])+0.5-P[j]/2,y=max(LL.pick[!is.na(LL.pick)]),labels=round(Rhythms.mean[ix],2))
+               if(j==1) text(x=0,y=max(LL.pick[!is.na(LL.pick)])/2,labels=round(Rhythms.mean[ix],2))
+         }
+    }
+
     return(list(Steps=LL,Rhythms.mean=Rhythms.mean))
 }
 
